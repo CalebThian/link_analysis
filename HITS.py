4f52c830e1e2f1ade37a893302cf08c17d5893e4
CalebThian <calebthian@gmail.com>
2022-12-20 15:28:10 +0800

    update HITS by using networkx

--- HITS.py
+++ HITS.py
@@ -3,19 +3,33 @@ import numpy as np
 
 path = "./data/graph_6.txt"
 
-def HITS(adj_mat,n,e):
-    adj_mat = np.array(adj_mat)
+def HITS(G):
+    n = len(G.nodes)
     last_aut = np.ones(n)
     last_hub = np.ones(n)
     aut = np.ones(n)
     hub = np.ones(n)
     eps = 0.001
-    t = 1
+    
+    index = dict()
+    t = 0
+    for node in sorted(list(G.nodes)):
+        index[node] = t
+        t += 1
+        
     while True:
-
-        for i in range(n):
-            aut[i] = np.sum(adj_mat.transpose()[i]*last_hub)
-            hub[i] = np.sum(adj_mat[i]*last_aut)
+        aut = np.zeros(n)
+        hub = np.zeros(n)
+        for node in G.nodes:
+            par = list(G.predecessors(node))
+            chi = list(G.successors(node))
+            for p in par:
+                aut[index[node]] += last_hub[index[p]]
+            for c in chi:
+                hub[index[node]] += last_aut[index[c]]
+            
+            #aut[i] = np.sum(adj_mat.transpose()[i]*last_hub)
+            #hub[i] = np.sum(adj_mat[i]*last_aut)
           
         #Normalization
         aut = aut/np.sum(aut)
@@ -38,9 +52,11 @@ def output_file(aut,hub,path):
     np.savetxt(h_file, aut, fmt='%3f', delimiter=" ",newline=' ')
     
 if __name__ == "__main__":
-    n,e = data.get_n_e(path)
-    data = data.get_amat(path)
-    aut,hub = HITS(data,n,e)
+    #n,e = data.get_n_e(path)
+    #data = data.get_amat(path)
+    #aut,hub = HITS(data,n,e)
+    G = data.get_graph(path)
+    aut,hub = HITS(G)
     print(aut)
     print(hub)
     output_file(aut,hub,path)
