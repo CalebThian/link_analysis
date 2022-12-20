f098d846a8cd9b5ed0fd8fde116bf867d39f228c
CalebThian <calebthian@gmail.com>
2022-12-20 08:56:37 +0800

    create adj_mat

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
-import config
-path = "./data/graph_1.txt"
+from config import g_info
+import numpy as np
+
+data_path = "./data/"
+file = "graph_1.txt"
 
 def read_files(path):
     with open (path, "r") as myfile:
@@ -10,11 +13,13 @@ def read_files(path):
         data[i][1] = int(data[i][1])
     return data
 
-def gen_graph(data):
-    print(config.g_info)
-    return
+def gen_graph(data,n,e):
+    adj_mat = np.zeros((n,n))
+    for d in data:
+        adj_mat[d[0]-1][d[1]-1] = 1
+    return adj_mat
 
 if __name__=="__main__":
-    data = read_files(path)
-    print(data)
-    gen_graph(data)
+    n,e = g_info[file][0],g_info[file][1]
+    data = read_files(data_path+file)
+    adj_mat = gen_graph(data,n,e)
