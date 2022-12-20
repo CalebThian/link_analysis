b6ea24e9c90d537070c955e8608e25201b09e888
CalebThian <calebthian@gmail.com>
2022-12-20 08:52:30 +0800

    add config

--- data.py
+++ data.py
@@ -1,6 +1,20 @@
+import config
 path = "./data/graph_1.txt"
 
-if __name__=="__main__":
+def read_files(path):
     with open (path, "r") as myfile:
         data = myfile.read().splitlines()
-        print(data)
+    for i in range(len(data)):
+        data[i] = data[i].split(",")
+        data[i][0] = int(data[i][0])
+        data[i][1] = int(data[i][1])
+    return data
+
+def gen_graph(data):
+    print(config.g_info)
+    return
+
+if __name__=="__main__":
+    data = read_files(path)
+    print(data)
+    gen_graph(data)
